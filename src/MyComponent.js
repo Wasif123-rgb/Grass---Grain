f2f5e600ab1a35783780340ed0762f67daf5fe77
Wasif123-rgb <wasif123ahmed123@gmail.com>
2026-04-12 00:01:57 +0600

    updated

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { CarbonFootprint } from "react-carbon-footprint";
+
+function MyComponent() {
+  return (
+    <div>
+      <h2>Carbon Footprint Tracker</h2>
+      <CarbonFootprint />
+    </div>
+  );
+}
+
+export default MyComponent;
